5a7eec8bec2bdcf45d85d46692796dc087fa8bda
Oliver Middleton <olliemail27@gmail.com>
2016-08-14 18:29:09 +0100

    update10
    d38f43a rustdoc: Don't include the path for primitive methods in the search results
    a5bac39 rustdoc: Fix links to static items in the search results
    1df203c rustdoc: Fix const background color
    82c9719 rustdoc: Remove item types from some title pages
    e5a0efe rustdoc: Don't add "some fields omitted" for fully opaque structs

--- implementors/core/ops/trait.Add.js
+++ implementors/core/ops/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["collections"] = ["impl&lt;'a&gt; <a class='trait' href='core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;&amp;'a str&gt; for <a class='struct' href='collections/string/struct.String.html' title='collections::string::String'>String</a>",];implementors["core"] = [];implementors["rustc_unicode"] = [];
+implementors["rustc_unicode"] = [];implementors["collections"] = ["impl&lt;'a&gt; <a class='trait' href='core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;&amp;'a str&gt; for <a class='struct' href='collections/string/struct.String.html' title='collections::string::String'>String</a>",];implementors["core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
