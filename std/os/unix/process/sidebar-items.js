251d7921aa220e04079c9f048b8bce63ed06244e
Oliver Middleton <olliemail27@gmail.com>
2018-01-27 10:26:12 +0000

    update to 5c41fcec4cd6e76180edb088a48914aa71612e5d

--- std/os/unix/process/sidebar-items.js
+++ std/os/unix/process/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["CommandExt","Unix-specific extensions to the `std::process::Command` builder"],["ExitStatusExt","Unix-specific extensions to `std::process::ExitStatus`"]]});
+initSidebarItems({"fn":[["parent_id","Returns the OS-assigned process identifier associated with this process's parent."]],"trait":[["CommandExt","Unix-specific extensions to the `std::process::Command` builder"],["ExitStatusExt","Unix-specific extensions to `std::process::ExitStatus`"]]});
